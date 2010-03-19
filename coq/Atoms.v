39e580abbf5e2af68645e84b139614773313e7e3
puech <puech@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2010-03-19 23:14:17 +0000

    Oups, mauvaise version...
    Au moins maintenant ça compile jusqu'au bout.
    
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@35 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- coq/Atoms.v
+++ coq/Atoms.v
@@ -17,44 +17,29 @@ Definition arity_of_atom (a : atom_name) : list atom :=
     | E => [A []; C [1]]
   end.
 
-  
 Definition env := list atom.
 
-Reserved Notation "Γ ⊢ [ n ] = a" (at level 59, n at next level).
-
-Inductive list_nth {T} : nat -> list T -> T -> Prop :=
-| Nth_o (a:T) Γ : 
-  (a :: Γ) ⊢ [0] = a
-
-| Nth_s m (a b:T) Γ : 
-  Γ ⊢ [m] = a ->
-  (b :: Γ) ⊢ [S m] = a
-
-where "Γ ⊢ [ n ] = a" := (@list_nth _ n Γ a).
-
-Reserved Notation "Γ ⊢ a" (at level 50, a at next level).
-Reserved Notation "Γ ⊢ vs ≡ As" (at level 50, vs at next level).
+Inductive list_nth {A} : nat -> list A -> A -> Prop :=
+| Nth_o a tl : list_nth 0 (a::tl) a
+| Nth_s m a b tl : 
+  list_nth m tl a ->
+  list_nth (S m) (b :: tl) a
+  .
 
 Inductive wt_atom (Γ : env) : atom -> Prop :=
 | Wt_atom A xs :
-  Γ ⊢ xs ≡ arity_of_atom A ->
-  Γ ⊢ A xs
-  where "Γ ⊢ a" := (wt_atom Γ a)
+  wt_atoms Γ xs (arity_of_atom A) ->
+  wt_atom Γ (A xs)
 
 with wt_atoms (Γ : env) : list var -> list atom -> Prop :=
 | Wt_atoms_nil :
-  Γ ⊢  [] ≡ []
+  wt_atoms Γ [] []
 | Wt_atoms_cons x xs a As :
-  Γ ⊢ [x] = a ->
   list_nth x Γ a ->
   wt_atom Γ a ->
   wt_atoms Γ xs As ->
   wt_atoms Γ (x :: xs) (a :: As)
-  where "Γ ⊢ vs ≡ As" := (wt_atoms Γ vs As)
-.
-
-Notation "Γ ⊢ vs ≡ As" := (wt_atoms Γ vs As) (at level 70).
-Notation "Γ ⊢ a" := (wt_atom Γ a) (at level 70).
+  .
 
 Inductive wt_atom_list (Γ : env) : list atom -> Prop :=
 | Wt_atom_list_nil :
@@ -65,12 +50,12 @@ Inductive wt_atom_list (Γ : env) : list atom -> Prop :=
   wt_atom_list Γ (a :: As)
 .
 
-Notation "Γ ⊧ As" := (wt_atom_list Γ As) (at level 70).
-
 Hint Constructors atom_name atom list_nth wt_atom wt_atoms wt_atom_list.
 
-Lemma ex_wt_atom_list : [] ⊧ [A[]; C[0]; E[1;0]].
+Lemma ex_wt_atom_list : wt_atom_list [] [A[]; C[0]; E[1;0]].
  apply Wt_atom_list_cons. auto.
  apply Wt_atom_list_cons. apply Wt_atom; simpl; auto.
  apply Wt_atom_list_cons. apply Wt_atom; simpl; auto.
- apply Wt_atom_list_cons. auto.
+ apply Wt_atoms_cons; auto.
+ apply Wt_atoms_cons; auto.     (* pas possible, bug de dB *)
+Abort.
--- coq/Meta.v
+++ coq/Meta.v
@@ -10,7 +10,6 @@ Hint Unfold decidable eq_dec.
 Module Type S.
   Parameter atom_name : Type.
   (* Parameter atom_name_eq_dec : eq_dec atom_name. *)
-  Parameter arity_of_atom : atom_name -> list atom_name.
   Parameter transformer : Type.
   (* Parameter transformer_eq_dec : eq_dec transformer. *)
 End S.
@@ -22,7 +21,9 @@ Module Type F1 (Import X : S).
   
   Inductive atom :=
   | Atom (A : atom_name) (xs : list var).
-  
+
+  Coercion Atom : atom_name >-> Funclass.
+
   Inductive judgement :=
   | Jdecl (atom : atom)
   | Jassign (atoms : list atom) (T : transformer) (args : list var).
@@ -34,12 +35,13 @@ Module Type F1 (Import X : S).
 End F1.
 
 Module Type F2 (Import X : S) (Import Y : F1 X).
+  Parameter arity_of_atom : atom_name -> list atom.  
   Parameter arity_of_transformer : transformer -> arity.
 End F2.
 
 Module Type F12 (X : S) := F1 X <+ F2 X.
 
-Module Make (Import X : S) (Import Y : F12 X).
+Module F3 (Import X : S) (Import Y : F12 X).
   
   Definition env := list atom.
   
@@ -52,17 +54,26 @@ Module Make (Import X : S) (Import Y : F12 X).
 
   Inductive wt_atom (Γ : env) : atom -> Prop :=
   | Wt_atom A xs :
-    wt_atom_list Γ xs (arity_of_atom A) ->
-    wt_atom Γ (Atom A xs)
-    
-  with wt_atom_list (Γ : env) : list var -> list atom_name -> Prop :=
+    wt_atoms Γ xs (arity_of_atom A) ->
+    wt_atom Γ (A xs)
+
+  with wt_atoms (Γ : env) : list var -> list atom -> Prop :=
+  | Wt_atoms_nil :
+    wt_atoms Γ [] []
+  | Wt_atoms_cons x xs a As :
+    list_nth x Γ a ->
+    wt_atom Γ a ->
+    wt_atoms Γ xs As ->
+    wt_atoms Γ (x :: xs) (a :: As)
+    .
+
+  Inductive wt_atom_list (Γ : env) : list atom -> Prop :=
   | Wt_atom_list_nil :
-    wt_atom_list Γ [] []
-  | Wt_atom_list_cons x xs ys A As :
-    list_nth x Γ (Atom A ys) ->
-    wt_atom Γ (Atom A ys) ->
-    wt_atom_list Γ xs As ->
-    wt_atom_list Γ (x :: xs) (A :: As)
+    wt_atom_list Γ []
+  | Wt_atom_list_cons a As :
+    wt_atom Γ a ->
+    wt_atom_list (a :: Γ) As ->
+    wt_atom_list Γ (a :: As)
     .
   
   Definition renaming := list var.
@@ -96,9 +107,9 @@ Module Make (Import X : S) (Import Y : F12 X).
 
   | Wt_args_decl σ x xs ys A zs js σ' Γ':
     rename_vars zs σ ys ->
-    list_nth x Γ (Atom A ys) -> 
-    wt_args (Atom A ys :: Γ) (x::σ) xs js σ' Γ' ->
-    wt_args Γ σ (x::xs) (Jdecl (Atom A zs) :: js) σ' Γ'
+    list_nth x Γ (A ys) -> 
+    wt_args (A ys :: Γ) (x::σ) xs js σ' Γ' ->
+    wt_args Γ σ (x::xs) (Jdecl (A zs) :: js) σ' Γ'
 
   | Wt_args_assign σ xs concls T args js σ' Γ' :
     wt_assign Γ σ args T concls σ' Γ' ->
@@ -113,8 +124,8 @@ Module Make (Import X : S) (Import Y : F12 X).
 
   | Wt_concls_decl σ A xs ys ats js σ' Γ':
     rename_vars xs σ ys ->      (* ptet l'inverse, ptet autre chose... *)
-    wt_concls (Atom A ys :: Γ) (0 :: σ) ats js σ' Γ' -> (* substitution ss doute fausse *)
-    wt_concls Γ σ (Atom A xs :: ats) (Jdecl (Atom A ys) :: js) σ' Γ'
+    wt_concls (A ys :: Γ) (0 :: σ) ats js σ' Γ' -> (* substitution ss doute fausse *)
+    wt_concls Γ σ (A xs :: ats) (Jdecl (A ys) :: js) σ' Γ'
 
   (* TODO assign *)
     .
@@ -134,85 +145,92 @@ Module Make (Import X : S) (Import Y : F12 X).
     wt_judgements Γ (Jassign concls T args :: js)
     .
 
-  Hint Constructors atom judgement arity list_nth wt_atom
+  Hint Constructors atom judgement arity list_nth wt_atom wt_atoms
     wt_atom_list rename_vars wt_judgements wt_assign wt_args wt_concls : patch.
 
-End Make.
+End F3.
 
 Module Import Example.
   Inductive an := A | B | C | D | E.
   Definition atom_name := an.
-  Definition arity_of_atom (a : atom_name) :=
-    match a with
-      | A => []   | B => []
-      | C => [A]  | D => [A;B]
-      | E => [A;B 0]
-    end.
-
   Inductive t := T | U | V.
   Definition transformer := t.
 
   Include F1.
 
+  (* Hack pour bénéficier de la coercion, a cause de la redirection an -> atom_name *)
+  Definition Atom2 : an -> list var -> atom := Atom.
+  Coercion Atom2 : an >-> Funclass.
+
+  Definition arity_of_atom (a : atom_name) : list atom :=
+    match a with
+      | A => []   | B => []
+      | C => [ A []]  | D => [A []; B []]
+      | E => [A []; C [1]]
+    end.
+
   Definition arity_of_transformer (t : transformer) := 
     match t with
       | T => 
-        {| ar_args := [Jdecl (Atom A []); Jdecl (Atom C [0])]; 
-           ar_concls := [Jdecl (Atom B []); Jdecl (Atom D [2;0])] |}
+        {| ar_args := [Jdecl (A []); Jdecl (C [0])]; 
+           ar_concls := [Jdecl (B []); Jdecl (D [2;0])] |}
       | U => {| ar_args := []; ar_concls := [] |}
       | V => {| ar_args := []; ar_concls := [] |}
     end.
 
-  Include Make.
+  Include F3.
 
   Ltac p := eauto with patch.
 
-  Lemma ex_atom : wt_atom [Atom B []; Atom A []] (Atom D [1;0]).
-        apply Wt_atom; eapply Wt_atom_list_cons; p.
+  Lemma ex_atom : wt_atom [B []; A []] (D [1;0]).
+        apply Wt_atom; eapply Wt_atoms_cons; p.
     (* impossible ac juste eauto?? pourquoi? *)
   Qed.
   
-  Lemma wf_atoms : well_founded (fun a b => In a (arity_of_atom b)).
-    red. intros.
-    cut (Acc (fun a b : atom_name => In a (arity_of_atom b)) A).
-    cut (Acc (fun a b : atom_name => In a (arity_of_atom b)) B).
-    induction a; intros; auto.
-    (* C *)
-    apply Acc_intro; intros. destruct H1; subst. trivial. destruct H1.
-    (* D *)
-    apply Acc_intro; intros. destruct H1; subst. trivial.
-    destruct H1; subst; trivial. destruct H1.
-    (* A *) apply Acc_intro; intros; destruct H; subst.
-    (* B *) apply Acc_intro; intros; destruct H; subst.
+  (* ce lemme *mériterait* une preuve plus compacte :) *)
+  Lemma wf_atoms : well_founded
+    (fun a b => match b with Atom b _ => In a (arity_of_atom b) end).
+    red; intros; induction a; induction A0; intros;
+      repeat try (apply Acc_intro; intros; destruct H; subst; try destruct H0).
   Qed.
 
   Lemma ex_wt_judgement_decls : wt_judgements []
-    [ Jdecl (Atom A []);
-      Jdecl (Atom B []);
-      Jdecl (Atom D [1;0]);
-      Jdecl (Atom C [2])
+    [ Jdecl (A []);
+      Jdecl (B []);
+      Jdecl (D [1;0]);
+      Jdecl (C [2])
     ].  (* Meme raison, on doit faire a la main *)
     apply Wt_judgements_cons; p. 
     apply Wt_judgements_cons; p.
     apply Wt_judgements_cons; p.
     apply Wt_judgements_cons; p.
-    apply Wt_atom; eapply Wt_atom_list_cons; p.
-    apply Wt_atom; eapply Wt_atom_list_cons; p.
+    apply Wt_atom; eapply Wt_atoms_cons; p.
+    apply Wt_atom; eapply Wt_atoms_cons; p.
   Qed.
 
   Lemma ex_wt_jdugement_decls2 : wt_judgements []
-    [ Jdecl (Atom A []);
-      Jdecl (Atom C [0]);
-      Jdecl (Atom D [1;0]);
-      Jdecl (Atom C [2])
+    [ Jdecl (A []);
+      Jdecl (C [0]);
+      Jdecl (D [1;0]);
+      Jdecl (C [2])
     ].  (* Meme raison, on doit faire a la main *)
+    apply Wt_judgements_cons; p.
+    apply Wt_judgements_cons; p.
+    apply Wt_judgements_cons; p.
+    apply Wt_judgements_cons; p.
+    apply Wt_atom; eapply Wt_atoms_cons; p.
+    apply Wt_atom; eapply Wt_atoms_cons; p.
+    eapply Wt_atoms_cons; p. p.
+    (* et en plus semble faux a cause des liftings pas faits *)
+  Abort.
 
-
-  Lemma ex_wt_assign : wt_assign [Atom C [1]; Atom B []; Atom A []] 
-    [] [2;0] T [Atom B []; Atom D[3;2]] [] 
-    [Atom A []; Atom B []; Atom C [1]; Atom B []; Atom D [3;2]].
+  (* Marche pas a cause des liftings pas faits *)
+  Lemma ex_wt_assign : wt_assign [C [1]; B []; A []]
+    [] [2;0] T [B []; D [3;2]] [] 
+    [A []; B []; C [1]; B []; D [3;2]].
     eapply Wt_assign; simpl.
     eapply Wt_args_decl. p. p.
-    eapply Wt_args_decl. p. p. apply Rename_cons. p. apply Rename_nil.
+    eapply Wt_args_decl. p. p. 
+  Abort.
 
 End Example.
