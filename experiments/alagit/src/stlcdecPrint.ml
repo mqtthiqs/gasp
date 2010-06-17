9cd6a7c26b4c7635fb22fa625f8b869bf2d0ac8d
yann <yann@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2010-06-17 14:53:27 +0000

    * Update pretty printer.
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@93 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- experiments/alagit/src/stlcdecPrint.ml
+++ experiments/alagit/src/stlcdecPrint.ml
@@ -3,36 +3,39 @@ open Format
 
 let rec declaration fmt = function
   | DValue (x, t, e) -> 
-    fprintf fmt "@[let %a : %a = @,@[%a@]@]" 
+    fprintf fmt "@[let %a : %a = @,@[<hov 2>%a@].@]" 
       identifier' x
       ty' t
       expression' e
   | DType tvar -> 
-    fprintf fmt "@[let type %a@]" 
+    fprintf fmt "@[let type %a.@]" 
       type_identifier' tvar
 
 and declarations fmt = function
   | EmptyDeclarations -> ()
-  | ConsDeclaration (d, ds) -> fprintf fmt "@[%a@\n%a@]" declaration' d declarations' ds
+  | ConsDeclaration (d, MetaAST.Object EmptyDeclarations) -> 
+    fprintf fmt "%a" declaration' d 
+  | ConsDeclaration (d, ds) -> 
+    fprintf fmt "%a@ %a" declaration' d declarations' ds
 
 and expression fmt = function
   | Var x -> 
-    fprintf fmt "%a" identifier' x
+    fprintf fmt "@[%a@]" identifier' x
   | Lam (x, t, e) -> 
-    fprintf fmt "(fun (%a: %a) -> %a)" 
+    fprintf fmt "@[(fun (%a: %a) ->@ @[%a@])@]" 
       identifier' x
       ty' t
       expression' e
   | App (e1, e2) -> 
-    fprintf fmt "(%a %a)" 
+    fprintf fmt "@[(%a %a)@]" 
       expression' e1
       expression' e2
 
 and ty fmt = function
   | TyVar x -> 
-    fprintf fmt "%a" type_identifier' x
+    fprintf fmt "@[%a@]" type_identifier' x
   | TyArrow (ty1, ty2) -> 
-    fprintf fmt "(%a -> %a)" 
+    fprintf fmt "@[(%a -> %a)@]" 
       ty' ty1
       ty' ty2
 
@@ -43,26 +46,32 @@ and type_identifier fmt x =
   fprintf fmt "%s" x
 
 and typing_environment fmt (Env bs) = 
-  bindings' fmt bs
+  match bs with 
+    | MetaAST.Object NoBinding -> ()
+    | _ -> 
+      fprintf fmt "@[<v 0>@[<v 2>import@ %a@]@ in@]" bindings' bs 
 
-and fragment fmt (Fragment (tenv, decs)) = 
-  fprintf fmt "@[%a@\n@\n%a@]@."
+and fragment fmt (Fragment (tenv, decs)) =
+  fprintf fmt "@[<v 2>%a@ @[<v 2>%a@]@]@."
     typing_environment' tenv
     declarations' decs
 
 and binding fmt = function
   | BindVar (x, t) -> 
-    fprintf fmt "@[val %a : %a@]" 
+    fprintf fmt "@[val %a : %a.@]" 
       identifier' x
       ty' t
 
   | BindTyVar tvar -> 
-    fprintf fmt "@[type %a@]" 
+    fprintf fmt "@[type %a.@]" 
       type_identifier' tvar
 
 and bindings fmt = function
   | NoBinding -> ()
-  | ConsBinding (b, bs) -> fprintf fmt "@[%a@\n%a@]" binding' b bindings' bs
+  | ConsBinding (b, MetaAST.Object NoBinding) -> 
+    fprintf fmt "%a" binding' b 
+  | ConsBinding (b, bs) -> 
+    fprintf fmt "%a@ %a" binding' b bindings' bs
 
 and bindings'           fmt = MetaPrint.on fmt bindings 
 and binding'            fmt = MetaPrint.on fmt binding 
