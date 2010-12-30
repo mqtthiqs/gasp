ad3ad6f90ae4edb2e34a21ff116d633f662b86fe
puech <puech@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2010-12-30 12:30:51 +0000

    Reversible commit to test NLF_check.
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@206 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- src/gasp.ml
+++ src/gasp.ml
@@ -30,22 +30,34 @@ let parse_buffer b filename =
 let down = SLF_LF.sign [] $ LF_XLF.sign [] $ XLF_XLFe.sign $ XLFe_NLF.sign NLF.NLFSign.empty
 let up = XLFe_NLF.from_sign $ XLF_XLFe.from_sign $ LF_XLF.from_sign $ SLF_LF.from_sign
 
+(* let _ = *)
+(*   (\* Parsing of the input file *\) *)
+(*   let b = Util.buffer_of_file (List.hd filenames) in *)
+(*   let s = parse_buffer b (List.hd filenames) in *)
+(*   (\* Print parsed file *\) *)
+(*   SLF_pp.sign Format.std_formatter s; *)
+(*   print_string "=================================\n"; *)
+(*   (\* First down & up *\) *)
+(*   let s' = (down $ up) s in *)
+(*   (\* Printing/re-parsing of s' to/from a buffer *\) *)
+(*   Buffer.reset b; *)
+(*   SLF_pp.sign (Format.formatter_of_buffer b) s'; *)
+(*   let s' = parse_buffer b "generated" in *)
+(*   (\* Second down & up *\) *)
+(*   let s' = (down $ up) s' in *)
+(*   (\* Printing of s' *\) *)
+(*   SLF_pp.sign Format.std_formatter s'; *)
+(*   (\* Type-checking of NLF *\) *)
+(*   let s'' = down s' in *)
+(*   NLF_check.sign s''; *)
+(*   (\* Comparison of the initial and final signatures *\) *)
+(*   if SLF.equals_sign SLF.Idmap.empty s s' then exit 0 else exit 1 *)
+
 let _ =
   (* Parsing of the input file *)
   let b = Util.buffer_of_file (List.hd filenames) in
   let s = parse_buffer b (List.hd filenames) in
-  (* Print parsed file *)
-  SLF_pp.sign Format.std_formatter s;
-  print_string "=================================\n";
-  (* First down & up *)
-  let s' = (down $ up) s in
-  (* Printing/re-parsing of s' to/from a buffer *)
-  Buffer.reset b;
-  SLF_pp.sign (Format.formatter_of_buffer b) s';
-  let s' = parse_buffer b "generated" in
-  (* Second down & up *)
-  let s' = (down $ up) s' in
-  (* Printing of s' *)
-  SLF_pp.sign Format.std_formatter s';
-  (* Comparison of the initial and final signatures *)
-  if SLF.equals_sign SLF.Idmap.empty s s' then exit 0 else exit 1
+  let s' = down s in
+  let s'' = up s' in
+  SLF_pp.sign Format.std_formatter s'';
+  NLF_check.sign s';
