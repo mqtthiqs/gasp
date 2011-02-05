209b65901e54564e350d00ce9a619eaab2c04d9b
puech <puech@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2011-02-05 18:19:17 +0000

    LF_check: solve substitution bug; print trace only when --debug.
    Conversion is done between *closures*: two terms and for each its substitution. At an application node, we generate two substitutions substl and substr, threading the one obtained upstream (subst). What we return to the node up is substl enriched with the new binding
    
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@260 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- src/LF_check.ml
+++ src/LF_check.ml
@@ -33,58 +33,56 @@ module Ren = struct
   let add x y r = match x,y with
     | Name.Named x, Name.Named y -> M.add x y r
     | _ -> r
+  let pp fmt subst = M.iter (Format.fprintf fmt "(%s/%s)") subst
   let empty = M.empty
 end
-
-let rec conv_obj subst ren t u = 
+  
+let rec conv_obj substl substr ren t u = 
   match t, u with
     | OMeta _, OMeta _ -> assert false
     | OConst x, OConst y when x=y -> ()
     | OVar x, _ -> 
 	let x = Ren.find x ren in
-	begin try conv_obj (Subst.remove x subst) ren (Subst.find x subst) u
+	begin try conv_obj (Subst.remove x substl) substr ren (Subst.find x substl) u
 	with Not_found -> 
 	  match u with OVar y when x = y -> () | _ -> assert false
 	end
     | _, OVar y ->
-	begin try conv_obj (Subst.remove y subst) ren t (Subst.find y subst)
+	begin try conv_obj substl (Subst.remove y substr) ren t (Subst.find y substr)
 	with Not_found ->
 	  match t with OVar x when x = y -> () | _ ->  assert false
 	end
     | OApp(t,u), OApp(t',u') ->
-	conv_obj subst ren t t';
-	conv_obj subst ren u u'
+	conv_obj substl substr ren t t';
+	conv_obj substl substr ren u u'
     | _ -> assert false
 
-let rec conv_fam subst ren a b =
+let rec conv_fam substl substr ren a b =
   match a, b with
     | FConst c, FConst d when c = d -> ()
     | FProd(x,a,b), FProd(y,c,d) ->
-	conv_fam subst ren a c;
-	conv_fam subst (Ren.add x y ren) b d
-    | FApp (a,t), FApp(b,u) -> conv_fam subst ren a b; conv_obj subst ren t u
+	conv_fam substl substr ren a c;
+	conv_fam substl substr (Ren.add x y ren) b d
+    | FApp (a,t), FApp(b,u) -> conv_fam substl substr ren a b; conv_obj substl substr ren t u
     | _ -> Errors.not_convertible_fam (SLF_LF.from_fam a) (SLF_LF.from_fam b)
 
-let rec conv_kind subst ren k l = 
+let rec conv_kind substl substr ren k l = 
   match k,l with
     | KType, KType -> ()
     | KProd(x,a,k), KProd(y,b,l) -> 
-	conv_fam subst ren a b;
-	conv_kind subst (Ren.add x y ren) k l
+	conv_fam substl substr ren a b;
+	conv_kind substl substr (Ren.add x y ren) k l
     | _ -> assert false
 
-let conv_obj subst t u = 
-  Format.printf "= CO %a ⊢ %a == %a@." 
-    Subst.pp subst SLF_pp.term (SLF_LF.from_obj t) SLF_pp.term (SLF_LF.from_obj u);
-  conv_obj subst Ren.empty t u
-let conv_fam subst a b = 
-  Format.printf "= CF %a ⊢ %a == %a@."
-    Subst.pp subst SLF_pp.term (SLF_LF.from_fam a) SLF_pp.term (SLF_LF.from_fam b);
-  conv_fam subst Ren.empty a b
-let conv_kind subst k l = 
-  Format.printf "= CK %a ⊢ %a == %a@."
-    Subst.pp subst SLF_pp.term (SLF_LF.from_kind k) SLF_pp.term (SLF_LF.from_kind l);
-  conv_kind subst Ren.empty k l
+let conv_obj substl substr t u = 
+  Util.if_debug (fun () -> Format.printf "= CO %a %a == %a %a@." SLF_pp.term (SLF_LF.from_obj t) Subst.pp substl SLF_pp.term (SLF_LF.from_obj u) Subst.pp substr);
+  conv_obj substl substr Ren.empty t u
+let conv_fam substl substr a b = 
+  Util.if_debug (fun () -> Format.printf "= CF %a %a == %a %a@." SLF_pp.term (SLF_LF.from_fam a) Subst.pp substl SLF_pp.term (SLF_LF.from_fam b) Subst.pp substr);
+  conv_fam substl substr Ren.empty a b
+let conv_kind substl substr k l = 
+  Util.if_debug (fun () -> Format.printf "= CK %a %a == %a %a@." SLF_pp.term (SLF_LF.from_kind k) Subst.pp substl SLF_pp.term (SLF_LF.from_kind l)Subst.pp substr);
+  conv_kind substl substr Ren.empty k l
 
 let rec obj' sign env subst t : fam * subst = 
   match t with
@@ -95,30 +93,23 @@ let rec obj' sign env subst t : fam * subst =
   | OVar x -> List.assoc x env, subst
   | OLam (x,a,t) -> 
       let k, _ = fam sign env subst a in
-      conv_kind subst k KType;
+      conv_kind subst Subst.empty k KType;
       let b, _ = obj sign ((Name.variable_for x,a)::env) subst t in
       FProd(x,a,b), subst
   | OApp (t,u) ->
       begin match obj sign env subst t with
-	| FProd(x,a,b), subst ->
-	    (let a', _ = obj sign env subst u in
-	    conv_fam subst a a');
-	    b, Subst.add_name x u subst
+	| FProd(x,a,b), substl ->
+	    (let a', substr = obj sign env subst u in
+	    conv_fam substl substr a a');
+	    b, Subst.add_name x u substl
 	| _ -> Errors.bad_application (SLF_LF.from_obj t)
       end
   | OMeta _ -> assert false
 
-and obj sign env subst t =
-  Format.printf "[ O %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_obj t);
-  let a, subst' = obj' sign env subst t in
-  Format.printf "] O %a : %a => %a@." SLF_pp.term (SLF_LF.from_obj t) SLF_pp.term (SLF_LF.from_fam a) Subst.pp subst;
-  a, subst'
-
-
 and fam' sign env subst = function
   | FProd(x,a,b) -> 
       let k, _ = fam sign env subst a in 
-      conv_kind subst k KType; 
+      conv_kind subst Subst.empty k KType; 
       fam sign ((Name.variable_for x,a)::env) subst b
   | FConst c -> begin match NLFSign.find sign c with
       | NLFSign.FDecl k -> reify_kind k, subst
@@ -126,27 +117,31 @@ and fam' sign env subst = function
     end
   | FApp (a,t) -> 
       match fam sign env subst a with
-	| KProd(x,a,k), subst -> 
-	    (let a', subst = obj sign env subst t in
-	     conv_fam subst a a');
-	    k, Subst.add_name x t subst
+	| KProd(x,a,k), substl -> 
+	    (let a', substr = obj sign env subst t in
+	     conv_fam substl substr a a');
+	    k, Subst.add_name x t substl
 	| KType, _ -> assert false
 	    
-and fam sign env subst a =
-  Format.printf "[ F %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_fam a);
-  let k, subst' = fam' sign env subst a in
-  Format.printf "] F %a : %a => %a@." SLF_pp.term (SLF_LF.from_fam a) SLF_pp.term (SLF_LF.from_kind k) Subst.pp subst;
-  k, subst'
-
-let rec kind' sign env subst k =
+and kind' sign env subst k =
   match k with
     | KType -> ()
     | KProd(x,a,k) -> 
 	let k, subst = fam sign env subst a in
-	conv_kind Subst.empty k KType;
+	conv_kind subst Subst.empty k KType;
 	kind sign ((Name.variable_for x,a)::env) subst k
 
+and obj sign env subst t =
+  Util.if_debug (fun () -> Format.printf "[ O %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_obj t));
+  let a, subst' = obj' sign env subst t in
+    Util.if_debug (fun () -> Format.printf "] O %a : %a => %a@." SLF_pp.term (SLF_LF.from_obj t) SLF_pp.term (SLF_LF.from_fam a) Subst.pp subst);
+  a, subst'
+and fam sign env subst a =
+  Util.if_debug (fun () -> Format.printf "[ F %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_fam a));
+  let k, subst' = fam' sign env subst a in
+  Util.if_debug (fun () -> Format.printf "] F %a : %a => %a@." SLF_pp.term (SLF_LF.from_fam a) SLF_pp.term (SLF_LF.from_kind k) Subst.pp subst);
+  k, subst'
 and kind sign env subst k =
-  Format.printf "[ K %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_kind k);
+    Util.if_debug (fun () -> Format.printf "[ K %a ⊢ %a@." Subst.pp subst SLF_pp.term (SLF_LF.from_kind k));
   let () = kind' sign env subst k in
-  Format.printf "] K %a : ok." SLF_pp.term (SLF_LF.from_kind k)
+    Util.if_debug (fun () -> Format.printf "] K %a : ok.@." SLF_pp.term (SLF_LF.from_kind k))
