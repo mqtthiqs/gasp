31962c8fc14cce2f9ac2bc8f575a5123d20daae3
puech <puech@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2011-03-03 18:52:25 +0000

    Name: separate variables, constants and definitions (meta) in abstract types
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@309 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- src/name.ml
+++ src/name.ml
@@ -1,20 +1,36 @@
-type constant = string
-type variable = string
+include types of mli with
 
-type name =
-  | Named of variable
-  | Anonymous
+type variable = string and
+type constant = string and
+type definition = string and
+module Varmap = Map.Make(struct type t = variable let compare = Pervasives.compare end) and 
+module Constmap = Map.Make(struct type t = constant let compare = Pervasives.compare end) and
+module Defmap = Map.Make(struct type t = definition let compare = Pervasives.compare end) and
+module Pp = struct
+  open Format
+  let variable fmt x = fprintf fmt "%s" x
+  let constant fmt x = fprintf fmt "%s" x
+  let definition fmt x = fprintf fmt "%s" x
+end
 
-let gen_new, gen_init =
+let gen_new, gen_init, gen_status =
   let c = ref 0 in
   (fun () -> incr c; !c),
-  (fun n -> c := n)
+  (fun n -> c := n),
+  (fun () -> !c)
 
-let gen_name () = "x" ^ (string_of_int (gen_new()))
+let gen_variable () = "v" ^ (string_of_int (gen_new()))
+let gen_constant () = "c" ^ (string_of_int (gen_new()))
+let gen_definition () = "d" ^ (string_of_int (gen_new()))
+
+let mk_variable s = s
+let mk_constant s = s
+let mk_definition s = s
+let of_variable s = s
+let of_constant s = s
+let of_definition s = s
 
 let variable_for = function
   | Named x -> x
-  | Anonymous -> gen_name()
+  | Anonymous -> gen_variable()
 
-module Varmap = Map.Make(struct type t = variable let compare = Pervasives.compare end)
-module Constmap = Map.Make(struct type t = constant let compare = Pervasives.compare end)
--- src/name.mli
+++ src/name.mli
@@ -1,14 +1,34 @@
-type constant = string
-type variable = string
+type constant
+type variable
+type definition
+
+val gen_status : unit -> int
+val gen_init : int -> unit
+
+val gen_variable : unit -> variable
+val gen_constant : unit -> constant
+val gen_definition : unit -> definition
+
+val mk_variable : string -> variable
+val mk_constant : string -> constant
+val mk_definition : string -> definition
+val of_variable : variable -> string
+val of_constant : constant -> string
+val of_definition : definition -> string
 
 type name =
   | Named of variable
   | Anonymous
 
-val gen_new : unit -> int
-val gen_init : int -> unit
-val gen_name : unit -> variable
 val variable_for : name -> variable
 
 module Varmap : Map.S with type key = variable
 module Constmap : Map.S with type key = constant
+module Defmap : Map.S with type key = definition
+
+module Pp : sig
+  open Print
+  val variable : variable printing_fun
+  val constant : constant printing_fun
+  val definition : definition printing_fun
+end
