5c42b121ee62ddba68ff5dea7b6f4cbf03762aa5
puech <puech@403e655e-980a-47bd-8c60-1ee5348bb6b7>
2011-04-06 15:05:04 +0000

    XLFe_XLFn: implement changes suggested by Hugo: arguments in XLFw are in XLFe form
    git-svn-id: http://gasp.dev.regis-gianas.com/svn/trunk@372 403e655e-980a-47bd-8c60-1ee5348bb6b7

--- kernel/XLFe_XLFn.ml
+++ kernel/XLFe_XLFn.ml
@@ -14,7 +14,7 @@ module XLFw = struct
     | KProd of variable * fam * kind
 
   and fhead =
-    | FConst of constant * args
+    | FConst of constant * XLFe.args
 
   and fam =
     | FProd of variable * fam * fam
@@ -27,10 +27,9 @@ module XLFw = struct
   and obj =
     | OClos of variable * fam * XLFe.obj * Env.t
     | OMeta of definition * fhead
-    | OHead of ohead * args * fhead
-    | OBox of obj * variable * args
+    | OHead of ohead * XLFe.args * fhead * Env.t (* this env applies to the arguments only *)
+    | OBox of obj * variable * (variable * obj) list
 
-  and args = (variable * obj) list
 end
 
 module XLFe_XLFw = struct
@@ -40,56 +39,53 @@ module XLFe_XLFw = struct
     | XLFe.OHead(XLFe.HVar x,l,a) -> 
 	begin
 	  try let (t,f) = XLFw.Env.find x e in
-	      red e t f l        (* TODO: variable capture in u *)
-	  with Not_found -> XLFw.OHead(XLFw.HVar x, args e l, fhead e a)
+	      red e f (t,l)        (* TODO: variable capture in u *)
+	  with Not_found -> XLFw.OHead(XLFw.HVar x, l, fhead e a, e)
 	end
-    | XLFe.OHead(XLFe.HConst c,l,a) -> XLFw.OHead(XLFw.HConst c, args e l, fhead e a)
-    | XLFe.OHead(XLFe.HApp t,l,a) -> red e t e l
+    | XLFe.OHead(XLFe.HConst c,l,a) -> XLFw.OHead(XLFw.HConst c, l, fhead e a, e)
+    | XLFe.OHead(XLFe.HApp t,l,a) -> red e e (t,l)
     | XLFe.OMeta (x,a) -> XLFw.OMeta(x, fhead e a)
     | XLFe.OBox(t,p,s) -> XLFw.OBox(obj e t,p,List.map (fun x, t -> x, obj e t) s)
 
-  and red e t f = function
-    | [] -> obj f t
-    | (x,u) :: l ->
-      begin match obj f t with
-	| XLFw.OClos(y,a,v,g) ->
-	  assert(x=y);
-	  red e v (XLFw.Env.add x (u,e) g) l
-	| XLFw.OHead _ -> assert false 	(* bc a head is not a function (eta) *)
-	| XLFw.OMeta _ -> assert false	(* TODO error: applied meta forbidden *)
-	| XLFw.OBox _ -> assert false	(* TODO error: applied box forbidden *)
-      end
+  and red e f = function
+    | XLFe.OHead(h,l,a), [] -> obj f (XLFe.OHead(h,l,a))
+    | XLFe.OLam(x,a,t), (y,u) :: l ->
+      assert(x=y);
+      red e (XLFw.Env.add x (u,e) f) (t,l)
+    | _ -> assert false		 (* head, u::l -> head is not a function (eta)
+				  * lam, [] -> a function is always totally applied (eta)
+				  * meta|box, _ -> no applied meta/box TODO error *)
 
   and fhead e = function
-    | XLFe.FConst(c,l) -> XLFw.FConst(c, args e l)
+    | XLFe.FConst(c,l) -> XLFw.FConst(c, l)
 
   and fam e = function
     | XLFe.FProd(x,a,b) -> XLFw.FProd(x, fam e a, fam e b)
-    | XLFe.FHead(XLFe.FConst(c,l)) -> XLFw.FHead(XLFw.FConst(c, args e l))
-	
-  and args e l = List.map (fun (x,a) -> x, obj e a) l
-    
+    | XLFe.FHead(XLFe.FConst(c,l)) -> XLFw.FHead(XLFw.FConst(c, l))
+
   let rec kind = function
     | XLFe.KType -> XLFw.KType
     | XLFe.KProd(x,a,k) -> XLFw.KProd(x, fam XLFw.Env.empty a, kind k)
 end
 
-module XLFw_XLFn = struct
+module rec XLFw_XLFn : sig
+  val obj : XLFw.obj -> XLFn.obj
+  val fam : XLFw.fam -> XLFn.fam
+  val kind : XLFw.kind -> XLFn.kind
+end = struct
 
   let rec obj : XLFw.obj -> XLFn.obj = function
     | XLFw.OMeta(x,a) -> XLFn.OMeta(x, fhead a)
-    | XLFw.OHead(h,l,a,f) -> XLFn.OHead(ohead h, args l, fhead a)
-    | XLFw.OClos(x,a,t,f) -> XLFn.OLam(x, fam a, obj (XLFe_XLFw.obj f t)) (* TODO: Quand utiliser e ou f ? *)
+    | XLFw.OHead(h,l,a,f) -> XLFn.OHead(ohead h, XLFe_XLFn.args f l, fhead a)
+    | XLFw.OClos(x,a,t,f) -> XLFn.OLam(x, fam a, obj (XLFe_XLFw.obj f t))
     | XLFw.OBox (t,p,s) -> XLFn.OBox(obj t, p, List.map (fun x, t -> x, obj t) s)
 
   and ohead = function
     | XLFw.HVar x -> XLFn.HVar x
     | XLFw.HConst c -> XLFn.HConst c
 
-  and args l = List.map (fun (x,t) -> x, obj t) l
-
   and fhead = function
-    | XLFw.FConst(c,l) -> XLFn.FConst(c, args l)
+    | XLFw.FConst(c,l) -> XLFn.FConst(c, XLFe_XLFn.args XLFw.Env.empty l)
 
   and fam = function
     | XLFw.FProd(x,a,b) -> XLFn.FProd(x, fam a, fam b)
@@ -101,17 +97,24 @@ module XLFw_XLFn = struct
 
 end
 
-module XLFe_XLFn = struct
-  let obj t = XLFw_XLFn.obj (XLFe_XLFw.obj XLFw.Env.empty t)
-  let fam a = XLFw_XLFn.fam (XLFe_XLFw.fam XLFw.Env.empty a)
+and XLFe_XLFn : sig
+  val obj : XLFw.Env.t -> XLFe.obj -> XLFn.obj
+  val args : XLFw.Env.t -> XLFe.args -> XLFn.args
+  val fam : XLFw.Env.t -> XLFe.fam -> XLFn.fam
+  val kind : XLFe.kind -> XLFn.kind
+end = struct
+  let obj e t = XLFw_XLFn.obj (XLFe_XLFw.obj e t)
+  let args e l = List.map (fun x, t -> x, obj e t) l
+  let fam e a = XLFw_XLFn.fam (XLFe_XLFw.fam e a)
   let kind k = XLFw_XLFn.kind (XLFe_XLFw.kind k)
-
-  let entry kont nlfs = function 
-    | XLFe.ODecl a -> kont nlfs (XLFn.ODecl (fam a))
-    | XLFe.FDecl k -> kont nlfs (XLFn.FDecl (kind k))
 end
 
-include XLFe_XLFn
+let obj t = XLFe_XLFn.obj XLFw.Env.empty t
+let fam a = XLFe_XLFn.fam XLFw.Env.empty a
+let kind k = XLFe_XLFn.kind k
+let entry kont nlfs = function
+  | XLFe.ODecl a -> kont nlfs (XLFn.ODecl (fam a))
+  | XLFe.FDecl k -> kont nlfs (XLFn.FDecl (kind k))
 
 (* ... and back: *)
 
