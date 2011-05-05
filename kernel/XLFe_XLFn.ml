427162594c141a7150fcb074a058f604d802e399
Matthias Puech <puech@cs.unibo.it>
2011-05-05 18:05:45 +0200

    XLFe_XLFn: simplify reduction by caring only about terms (types/kinds come from previous languages)

--- kernel/XLFe_XLFn.ml
+++ kernel/XLFe_XLFn.ml
@@ -9,108 +9,103 @@ module XLFw = struct
     let empty = Env Varmap.empty
   end
 
-  type kind =
-    | KType
-    | KProd of variable * fam * kind
-
-  and fhead =
-    | FConst of constant * XLFe.args
-
-  and fam =
-    | FProd of variable * fam * fam
-    | FHead of fhead
-
-  and ohead =
+  type ohead =
     | HVar of variable
     | HConst of constant
 
   and obj =
-    | OClos of variable * fam * XLFe.obj * Env.t
-    | OMeta of definition * fhead
-    | OHead of ohead * XLFe.args * fhead * Env.t (* this env applies to the arguments only *)
+    | OClos of variable * XLFe.fam * XLFe.obj * Env.t
+    | OMeta of definition * XLFe.fhead
+    | OHead of ohead * XLFe.args * XLFe.fhead * Env.t (* this env applies to the arguments only *)
     | OBox of obj * variable * (variable * obj) list
 
 end
 
 module XLFe_XLFw = struct
 
-  let rec obj e : XLFe.obj -> XLFw.obj = function
-    | XLFe.OLam(x,a,t) -> XLFw.OClos(x, fam e a, t, e)
+  let rec obj' e : XLFe.obj -> XLFw.obj = function
+    | XLFe.OLam(x,a,t) -> XLFw.OClos(x, a, t, e)
     | XLFe.OHead(XLFe.HVar x,l,a) -> 
 	begin
 	  try let (t,f) = XLFw.Env.find x e in
-	      red e f (t,l)        (* TODO: variable capture in u *)
-	  with Not_found -> XLFw.OHead(XLFw.HVar x, l, fhead e a, e)
+	      red e f (t,List.rev l)        (* TODO: variable capture in u *)
+	  with Not_found -> XLFw.OHead(XLFw.HVar x, l, a, e)
 	end
-    | XLFe.OHead(XLFe.HConst c,l,a) -> XLFw.OHead(XLFw.HConst c, l, fhead e a, e)
-    | XLFe.OHead(XLFe.HApp t,l,a) -> red e e (t,l)
-    | XLFe.OMeta (x,a) -> XLFw.OMeta(x, fhead e a)
+    | XLFe.OHead(XLFe.HConst c,l,a) -> XLFw.OHead(XLFw.HConst c, l, a, e)
+    | XLFe.OHead(XLFe.HApp t,l,a) -> red e e (t, List.rev l)
+    | XLFe.OMeta (x,a) -> XLFw.OMeta(x, a)
     | XLFe.OBox(t,p,s) -> XLFw.OBox(obj e t,p,List.map (fun x, t -> x, obj e t) s)
 
-  and red e f = function
+  and red' e f = function
     | XLFe.OHead(h,l,a), [] -> obj f (XLFe.OHead(h,l,a))
     | XLFe.OLam(x,a,t), (y,u) :: l ->
-      assert(x=y);
+      (* Format.printf "red %a %a@." Name.Pp.variable x Name.Pp.variable y; *)
+      (* assert(x=y); *)    (* Pas forcément: la substitution ne préserve pas des noms valides *)
       red e (XLFw.Env.add x (u,e) f) (t,l)
+    | XLFe.OHead _, _ :: _ -> assert false
+    | XLFe.OLam _, [] -> assert false
     | _ -> assert false		 (* head, u::l -> head is not a function (eta)
 				  * lam, [] -> a function is always totally applied (eta)
 				  * meta|box, _ -> no applied meta/box TODO error *)
 
-  and fhead e = function
-    | XLFe.FConst(c,l) -> XLFw.FConst(c, l)
+  and obj e t =
+    Util.if_debug (fun () -> Format.printf "@[<v 2>{ O %a@," XLFa.Pp.obj (XLFa_XLFe.from_obj t));
+    let t = obj' e t in
+    Util.if_debug (fun () -> Format.printf "@]} O@,");
+    t
 
-  and fam e = function
-    | XLFe.FProd(x,a,b) -> XLFw.FProd(x, fam e a, fam e b)
-    | XLFe.FHead(XLFe.FConst(c,l)) -> XLFw.FHead(XLFw.FConst(c, l))
+  and red e f (t,l) =
+    Util.if_debug (fun () -> Format.printf "@[<v 2>{ R < %a | %a >@,"
+      XLFa.Pp.obj (XLFa_XLFe.from_obj t)
+      XLFa.Pp.args (XLFa_XLFe.from_args l)
+    );
+    let t = red' e f (t,l) in
+    Util.if_debug (fun () -> Format.printf "@]} R@,");
+    t
 
-  let rec kind = function
-    | XLFe.KType -> XLFw.KType
-    | XLFe.KProd(x,a,k) -> XLFw.KProd(x, fam XLFw.Env.empty a, kind k)
 end
 
 module rec XLFw_XLFn : sig
   val obj : XLFw.obj -> XLFn.obj
-  val fam : XLFw.fam -> XLFn.fam
-  val kind : XLFw.kind -> XLFn.kind
 end = struct
 
   let rec obj : XLFw.obj -> XLFn.obj = function
-    | XLFw.OMeta(x,a) -> XLFn.OMeta(x, fhead XLFw.Env.empty a)
-    | XLFw.OHead(h,l,a,f) -> XLFn.OHead(ohead h, XLFe_XLFn.args f l, fhead f a)
-    | XLFw.OClos(x,a,t,f) -> XLFn.OLam(x, fam a, obj (XLFe_XLFw.obj f t))
+    | XLFw.OMeta(x,a) -> XLFn.OMeta(x, XLFe_XLFn.fhead XLFw.Env.empty a)
+    | XLFw.OHead(h,l,a,f) -> XLFn.OHead(ohead h, XLFe_XLFn.args f l, XLFe_XLFn.fhead f a)
+    | XLFw.OClos(x,a,t,f) -> XLFn.OLam(x, XLFe_XLFn.fam a, obj (XLFe_XLFw.obj f t))
     | XLFw.OBox (t,p,s) -> XLFn.OBox(obj t, p, List.map (fun x, t -> x, obj t) s)
 
   and ohead = function
     | XLFw.HVar x -> XLFn.HVar x
     | XLFw.HConst c -> XLFn.HConst c
 
-  and fhead e = function
-    | XLFw.FConst(c,l) -> XLFn.FConst(c, XLFe_XLFn.args e l)
-
-  and fam = function
-    | XLFw.FProd(x,a,b) -> XLFn.FProd(x, fam a, fam b)
-    | XLFw.FHead h -> XLFn.FHead (fhead XLFw.Env.empty h)
-
-  let rec kind = function
-    | XLFw.KType -> XLFn.KType
-    | XLFw.KProd(x,a,k) -> XLFn.KProd(x, fam a, kind k)
-
 end
 
 and XLFe_XLFn : sig
   val obj : XLFw.Env.t -> XLFe.obj -> XLFn.obj
   val args : XLFw.Env.t -> XLFe.args -> XLFn.args
-  val fam : XLFw.Env.t -> XLFe.fam -> XLFn.fam
+  val fhead : XLFw.Env.t -> XLFe.fhead -> XLFn.fhead
+  val fam : XLFe.fam -> XLFn.fam
   val kind : XLFe.kind -> XLFn.kind
 end = struct
   let obj e t = XLFw_XLFn.obj (XLFe_XLFw.obj e t)
   let args e l = List.map (fun x, t -> x, obj e t) l
-  let fam e a = XLFw_XLFn.fam (XLFe_XLFw.fam e a)
-  let kind k = XLFw_XLFn.kind (XLFe_XLFw.kind k)
+
+  let fhead e = function
+    | XLFe.FConst(c,l) -> XLFn.FConst(c, args e l)
+
+  let rec fam = function
+    | XLFe.FProd(x,a,b) -> XLFn.FProd(x, fam a, fam b)
+    | XLFe.FHead h -> XLFn.FHead (fhead XLFw.Env.empty h)
+
+  let rec kind = function
+    | XLFe.KType -> XLFn.KType
+    | XLFe.KProd(x,a,k) -> XLFn.KProd(x, fam a, kind k)
+
 end
 
 let obj t = XLFe_XLFn.obj XLFw.Env.empty t
-let fam a = XLFe_XLFn.fam XLFw.Env.empty a
+let fam a = XLFe_XLFn.fam a
 let kind k = XLFe_XLFn.kind k
 let entry kont nlfs = function
   | XLFe.ODecl a -> kont nlfs (XLFn.ODecl (fam a))
