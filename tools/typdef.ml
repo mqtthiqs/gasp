1f9d627365b02e45c7e29969039ec2cd0368b915
Matthias Puech <puech@cs.unibo.it>
2011-05-30 01:41:35 +0200

    typdef: we can't use usual "with" constraints, so build our own datastructure.
    In camlp4, module constraints [with module M = a] take a _name_ a and not a module_expr. Using with_constr would diminuish the value of our tool...

--- tools/typdef.ml
+++ tools/typdef.ml
@@ -12,26 +12,28 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
 
   exception Unimplemented
 
+  type modtype_rewrite =
+    | MAnd of modtype_rewrite * modtype_rewrite
+    | MType of Ast.ctyp * Ast.ctyp
+    | MModule of Ast.ident * Ast.module_expr
+
   module Ident = struct
     type t = Ast.ident
     let rec compare x y = match x,y with
       | <:ident< $lid:i1$ >>, <:ident< $lid:i2$ >>
       | <:ident< $anti:i1$ >>, <:ident< $anti:i2$ >>
       | <:ident< $uid:i1$ >>, <:ident< $uid:i2$ >> ->
-	Printf.eprintf "compare %s <> %s\n" i1 i2;
 	String.compare i1 i2
       | <:ident< $a1$ . $b1$ >>, <:ident< $a2$ . $b2$ >>
       | <:ident< $a1$ $b1$ >>, <:ident< $a2$ $b2$ >> ->
-	(* compare a1 a2 * compare b1 b2 *)
-	assert false
+	compare a1 a2 * compare b1 b2
       | <:ident< $list:i1$ >>, <:ident< $list:i2$ >> ->
-	(* compare i1 i2 *)
-	assert false
+	compare i1 i2
   end
 
   module Sigma = struct
     module Identmap = Map.Make(Ident)
-    type t = Ast.ctyp Identmap.t * Ast.ident Identmap.t
+    type t = Ast.ctyp Identmap.t * Ast.module_expr Identmap.t
     let add_typ x t (tymap, modmap) = Identmap.add x t tymap, modmap
     let add_mod x t (tymap, modmap) = tymap, Identmap.add x t modmap
     let find_typ x (tymap, modmap) = Identmap.find x tymap
@@ -41,24 +43,20 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
       (Identmap.fold (fun _ _ n -> n + 1) modmap 0)
   end
 
-  let rec add_with_constr sigma = function
-    | <:with_constr< $w1$ and $w2$ >> ->
-      add_with_constr (add_with_constr sigma w2) w1
+  let rec build_rewrites sigma = function
+    | MAnd (w1, w2) -> build_rewrites (build_rewrites sigma w2) w1
     (* TODO: as in (see below): need to find a solution for general type parameters *)
-    | <:with_constr< type $id:id$ '$_$ = $td$ >>
-    | <:with_constr< type $id:id$ '$_$ '$_$ = $td$ >>
-    | <:with_constr< type $id:id$ '$_$ '$_$ '$_$ = $td$ >>
-    | <:with_constr< type $id:id$ '$_$ '$_$ '$_$ '$_$ = $td$ >>
-    | <:with_constr< type $id:id$ '$_$ '$_$ '$_$ '$_$ '$_$ = $td$ >>
-    | <:with_constr< type $id:id$ = $td$ >> ->
+    | MType(<:ctyp<$id:id$ '$_$>>,td)
+    | MType(<:ctyp<$id:id$ '$_$ '$_$>>,td)
+    | MType(<:ctyp<$id:id$ '$_$ '$_$ '$_$>>,td)
+    | MType(<:ctyp<$id:id$ '$_$ '$_$ '$_$ '$_$>>,td)
+    | MType(<:ctyp<$id:id$>>, td) ->
       Sigma.add_typ id td sigma
 
-    | <:with_constr@loc< type $_$ = $_$ >> -> raise (Loc.Exc_located (loc, Unimplemented)) (* TODO *)
+    | MType(<:ctyp<$_$>>, _) -> raise (Loc.Exc_located (Loc.ghost, Unimplemented)) (* TODO *)
 
-    | <:with_constr< module $id1$ = $id2$ >> ->
-      Sigma.add_mod id1 id2 sigma
-    | <:with_constr< >> -> sigma
-    | <:with_constr@loc< $anti:_$ >> -> raise (Loc.Exc_located (loc, Unimplemented))
+    | MModule(id, me) ->
+      Sigma.add_mod id me sigma
 
   let rec rewrite_sig_item path sigma : Ast.sig_item -> Ast.str_item = function
     | <:sig_item@loc< $s1$ ; $s2$ >> ->
@@ -66,7 +64,7 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
     | <:sig_item@loc< open $os$ >> -> <:str_item@loc< open $os$ >>
     | <:sig_item@loc< module $id$ : $mt$>> ->
       begin
-	try <:str_item@loc< module $id$ = $id:Sigma.find_mod (path id) sigma$ >>
+	try <:str_item@loc< module $id$ = $Sigma.find_mod (path id) sigma$ >>
         with Not_found ->
 	  let path y = <:ident@here< $path id$.$lid:y$ >> in
 	  <:str_item@loc< module $id$ = $rewrite_modtype path sigma mt$ >>
@@ -122,39 +120,53 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
   and rewrite_modtype path sigma : Ast.module_type -> Ast.module_expr = function
     | <:module_type@loc< sig $si$ end >> ->
       <:module_expr@loc< struct $rewrite_sig_item path sigma si$ end>>
-    | <:module_type@loc< $mt$ with $wc$ >> ->
-      let sigma = add_with_constr sigma wc in
-      Printf.eprintf "sigma contient %d bindings \n" (Sigma.cardinal sigma);
-      rewrite_modtype path (sigma) mt
     | <:module_type@loc< functor ($argn$ : $argt$) -> $mt$ >> ->
       <:module_expr@loc< functor ($argn$ : $argt$) -> $rewrite_modtype path sigma mt$ >>
     | <:module_type@loc< $id:id$ >> -> 
       raise (Loc.Exc_located (loc, Unimplemented))
     | <:module_type@loc< >> -> assert false
-    | _ -> raise (Loc.Exc_located (Loc.ghost, Unimplemented)) (* TODO complete *)
+    | <:module_type@loc< $_$ with $_$ >>
+    | <:module_type@loc< $anti:_$ >>
+    | <:module_type@loc< '$_$ >>
+      -> raise (Loc.Exc_located (loc, Unimplemented)) (* TODO complete *)
+
+  let modtype_rewrite = Gram.Entry.mk "modtype_rewrite"
 
   EXTEND Gram
+    modtype_rewrite:
+      [[ e1 = SELF; LIDENT "and"; e2 = SELF -> MAnd(e1,e2)
+       | "type"; t1 = ctyp; "="; t2 = ctyp -> MType(t1,t2)
+       | "module"; id = ident; "="; me = module_expr -> MModule(id,me)
+      ]];
+
     module_type:
-    [[ LIDENT "mli" ->
-      try
-	let nmli = (Filename.chop_extension (!Camlp4_config.current_input_file))^".mli" in
-	if nmli = !Camlp4_config.current_input_file then <:module_type<sig end>> else
-	  let mli = Syntax.parse_interf (Loc.mk nmli) (Stream.of_channel (open_in (nmli))) in
-	  <:module_type<sig $mli$ end>>
-      with Sys_error _ -> <:module_type<sig end>>
-    ]];
+      [[ LIDENT "mli" ->
+	begin
+	  try let nmli = (Filename.chop_extension (!Camlp4_config.current_input_file))^".mli" in
+	    if nmli = !Camlp4_config.current_input_file then <:module_type<sig end>> else
+	      let mli = Syntax.parse_interf (Loc.mk nmli) (Stream.of_channel (open_in (nmli))) in
+	      <:module_type<sig $mli$ end>>
+	  with Sys_error _ -> <:module_type<sig end>>
+	end
+       ]];
 
     module_expr:
-      [[ LIDENT "types"; "of"; e = module_type ->
-      rewrite_modtype (fun x -> <:ident< $lid:x$ >>) Sigma.empty e
-      ]];
-  END;;
+      [ [
+	"types"; "of"; e = module_type ->
+	rewrite_modtype (fun x -> <:ident< $lid:x$ >>) Sigma.empty e
+      |	"types"; "of"; e = module_type; "with"; r = modtype_rewrite ->
+	rewrite_modtype (fun x -> <:ident< $lid:x$ >>) (build_rewrites Sigma.empty r) e
+      ] ];
+  END
+  ;;
 
-    ErrorHandler.register
+  ErrorHandler.register
     (fun ppf -> function 
       | Unimplemented ->
         Format.fprintf ppf "%s: translation unimplemented for that kind of sig_item" Id.name
       | exn -> raise exn)
+  ;;
+
 end
 
 let module M = Register.OCamlSyntaxExtension(Id)(Make) in ()
