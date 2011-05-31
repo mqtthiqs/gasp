77196e137fece193249009eb827f8596c2154eaf
Matthias Puech <puech@cs.unibo.it>
2011-05-31 17:52:42 +0200

    new version of typdef, relying on Camlp4

--- tools/typdef.ml
+++ tools/typdef.ml
@@ -1,5 +1,5 @@
 module Id = struct
-  let name = "pa_typof"
+  let name = "pa_modof"
   let version = "1.0"
 end
 
@@ -28,16 +28,24 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
       | <:ident< $a1$ $b1$ >>, <:ident< $a2$ $b2$ >> ->
 	compare a1 a2 * compare b1 b2
       | <:ident< $list:i1$ >>, <:ident< $list:i2$ >> ->
-	compare i1 i2
+	Pervasives.compare i1 i2	(* TODO what's going on here?? *)
+
+    open Format
+    let rec print fmt = function
+      | <:ident< $lid:s$ >> -> printf "%s" s
+      | <:ident< $uid:s$ >> -> printf "%s" s
+      | <:ident< $anti:s$ >> -> printf "'%s" s
+      | <:ident< $a$ . $b$ >> -> printf "%a.%a" print a print b
+      | <:ident< $a$ $b$ >> -> printf "%a %a" print a print b
   end
 
   module Sigma = struct
     module Identmap = Map.Make(Ident)
     type t = Ast.ctyp Identmap.t * Ast.module_expr Identmap.t
-    let add_typ x t (tymap, modmap) = Identmap.add x t tymap, modmap
-    let add_mod x t (tymap, modmap) = tymap, Identmap.add x t modmap
-    let find_typ x (tymap, modmap) = Identmap.find x tymap
-    let find_mod x (tymap, modmap) = Identmap.find x modmap
+    let add_typ x t (tymap, modmap) = Format.eprintf "add_typ %a\n" Ident.print x; Identmap.add x t tymap, modmap
+    let add_mod x t (tymap, modmap) = Format.eprintf "add_mod %a\n" Ident.print x; tymap, Identmap.add x t modmap
+    let find_typ x (tymap, modmap) = Format.eprintf "find_typ %a\n" Ident.print x; Identmap.find x tymap
+    let find_mod x (tymap, modmap) = Format.eprintf "find_mod %a\n" Ident.print x; Identmap.find x modmap
     let empty = Identmap.empty, Identmap.empty
     let cardinal (tymap, modmap) = Identmap.fold (fun _ _ n -> n + 1) tymap 
       (Identmap.fold (fun _ _ n -> n + 1) modmap 0)
@@ -53,12 +61,49 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
     | MType(<:ctyp<$id:id$>>, td) ->
       Sigma.add_typ id td sigma
 
-    | MType(<:ctyp<$_$>>, _) -> raise (Loc.Exc_located (Loc.ghost, Unimplemented)) (* TODO *)
+    | MType(<:ctyp<$_$>>, _) -> raise Unimplemented (* TODO location *)
 
     | MModule(id, me) ->
       Sigma.add_mod id me sigma
 
-  let rec rewrite_sig_item path sigma : Ast.sig_item -> Ast.str_item = function
+  let rec rewrite_type path sigma : Ast.ctyp -> Ast.ctyp = Printf.eprintf "coucou\n"; function
+    | <:ctyp@loc< $lid:id$ = $t$ >> ->
+      Printf.eprintf "#### on cherche %s\n" id;
+      begin 
+	try <:ctyp@loc< $lid:id$ == $Sigma.find_typ (path id) sigma$ >>
+        with Not_found -> 
+	  Printf.eprintf "#### pas trouvé\n";	  
+	  <:ctyp@loc< $lid:id$ == $t$ >>
+      end
+
+    (* TODO: find a remedy for that: we need to repeat type declarations with 
+       1, 2, 3... parameters since there is no quotations for the n case *)
+    | <:ctyp@loc< $lid:id$ $p$ == $t$ >> ->
+      Printf.eprintf "#### on cherche %s\n" id;
+      begin
+    	try <:ctyp@loc< $lid:id$ $p$ == $Sigma.find_typ (path id) sigma$ >>
+        with Not_found -> 
+	  Printf.eprintf "#### pas trouvé\n";
+	  <:ctyp@loc< $lid:id$ $p$ == $t$ >>
+      end
+    | <:ctyp@loc< $lid:id$ $p$ $q$ == $t$ >> ->
+      begin
+    	try <:ctyp@loc< $lid:id$ $p$ $q$ == $Sigma.find_typ (path id) sigma$ >>
+        with Not_found -> <:ctyp@loc< $lid:id$ $p$ $q$ == $t$ >>
+      end
+    | <:ctyp@loc< $lid:id$ $p$ $q$ $r$ == $t$ >> ->
+      begin
+    	try <:ctyp@loc< $lid:id$ $p$ $q$ $r$ == $Sigma.find_typ (path id) sigma$ >>
+        with Not_found -> <:ctyp@loc< $lid:id$ $p$ $q$ $r$ == $t$ >>
+      end
+    (* end of TODO *)
+
+    | <:ctyp@loc< $typ:t1$ and $typ:t2$ >> ->
+      <:ctyp@loc< $rewrite_type path sigma t1$ and $rewrite_type path sigma t2$ >>
+
+    | t -> t				(* TODO why do we need that? *)
+
+  and rewrite_sig_item path sigma : Ast.sig_item -> Ast.str_item = function
     | <:sig_item@loc< $s1$ ; $s2$ >> ->
       <:str_item@loc< $rewrite_sig_item path sigma s1$; $rewrite_sig_item path sigma s2$ >>
     | <:sig_item@loc< open $os$ >> -> <:str_item@loc< open $os$ >>
@@ -69,38 +114,10 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
 	  let path y = <:ident@here< $path id$.$lid:y$ >> in
 	  <:str_item@loc< module $id$ = $rewrite_modtype path sigma mt$ >>
       end
-    | <:sig_item@loc< type $lid:id$ = $typ:t$ >> ->
-      Printf.eprintf "#### on cherche %s\n" id;
-      begin 
-	try <:str_item@loc< type $lid:id$ = $Sigma.find_typ (path id) sigma$ >>
-        with Not_found -> 
-	  Printf.eprintf "#### pas trouvé\n";	  
-	  <:str_item@loc< type $lid:id$ = $t$ >>
-      end
 
-    (* TODO: find a remedy for that: we need to repeat type declarations with 
-       1, 2, 3... parameters since there is no quotations for the n case *)
-    | <:sig_item@loc< type $lid:id$ $p$ = $t$ >> ->
-      Printf.eprintf "#### on cherche %s\n" id;
-      begin
-    	try <:str_item@loc< type $lid:id$ $p$ = $Sigma.find_typ (path id) sigma$ >>
-        with Not_found -> 
-	  Printf.eprintf "#### pas trouvé\n";
-	  <:str_item@loc< type $lid:id$ $p$ = $t$ >>
-      end
-    | <:sig_item@loc< type $lid:id$ $p$ $q$ = $t$ >> ->
-      begin
-    	try <:str_item@loc< type $lid:id$ $p$ $q$ = $Sigma.find_typ (path id) sigma$ >>
-        with Not_found -> <:str_item@loc< type $lid:id$ $p$ $q$ = $t$ >>
-      end
-    | <:sig_item@loc< type $lid:id$ $p$ $q$ $r$ = $t$ >> ->
-      begin
-    	try <:str_item@loc< type $lid:id$ $p$ $q$ $r$ = $Sigma.find_typ (path id) sigma$ >>
-        with Not_found -> <:str_item@loc< type $lid:id$ $p$ $q$ $r$ = $t$ >>
-      end
-    (* end of TODO *)
+    | <:sig_item@loc< type $t$ >> -> <:str_item@loc< type $rewrite_type path sigma t$ >>
 
-    | <:sig_item@loc< type $_$ >> -> raise (Loc.Exc_located (loc, Unimplemented)) (* TODO *)
+
     | <:sig_item@loc< module type $id$ = $mt$ >> -> <:str_item@loc< module type $id$ = $mt$ >>
     | <:sig_item@loc< exception $ts$ >> -> <:str_item@loc< exception $ts$ >>
     | <:sig_item@loc< include $mt$ >> -> 
@@ -110,7 +127,7 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
     | <:sig_item@loc< class $_$ >>
     | <:sig_item@loc< $anti:_$ >>
     | <:sig_item@loc< external $_$ : $_$ = $_$ >>
-    | <:sig_item@loc< class type $_$ >> -> raise (Loc.Exc_located (loc, Unimplemented))
+    | <:sig_item@loc< class type $_$ >> -> Loc.raise loc Unimplemented
 
     | <:sig_item@loc< # $_$ >>
     | <:sig_item@loc< # $_$ $_$ >>
@@ -122,13 +139,11 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
       <:module_expr@loc< struct $rewrite_sig_item path sigma si$ end>>
     | <:module_type@loc< functor ($argn$ : $argt$) -> $mt$ >> ->
       <:module_expr@loc< functor ($argn$ : $argt$) -> $rewrite_modtype path sigma mt$ >>
-    | <:module_type@loc< $id:id$ >> -> 
-      raise (Loc.Exc_located (loc, Unimplemented))
+    | <:module_type@loc< $id:id$ >> -> Loc.raise loc Unimplemented
     | <:module_type@loc< >> -> assert false
     | <:module_type@loc< $_$ with $_$ >>
     | <:module_type@loc< $anti:_$ >>
-    | <:module_type@loc< '$_$ >>
-      -> raise (Loc.Exc_located (loc, Unimplemented)) (* TODO complete *)
+    | <:module_type@loc< '$_$ >> -> Loc.raise loc Unimplemented (* TODO complete *)
 
   let modtype_rewrite = Gram.Entry.mk "modtype_rewrite"
 
@@ -152,9 +167,9 @@ module Make (Syntax : Sig.Camlp4Syntax) = struct
 
     module_expr:
       [ [
-	"types"; "of"; e = module_type ->
+	"module"; "of"; e = module_type ->
 	rewrite_modtype (fun x -> <:ident< $lid:x$ >>) Sigma.empty e
-      |	"types"; "of"; e = module_type; "with"; r = modtype_rewrite ->
+      |	"module"; "of"; e = module_type; "using"; r = modtype_rewrite ->
 	rewrite_modtype (fun x -> <:ident< $lid:x$ >>) (build_rewrites Sigma.empty r) e
       ] ];
   END
