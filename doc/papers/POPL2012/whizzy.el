410413277c68b3e33c03c0f9d5843de397447f4d
Yann Regis-Gianas <yrg@pps.jussieu.fr>
2011-06-27 14:13:06 +0200

    * Definition of openscope and expansion.

--- doc/papers/POPL2012/whizzy.el
+++ doc/papers/POPL2012/whizzy.el
@@ -4,5 +4,5 @@
 )
 (whizzy-add-configuration
  "main\.tex" 
- '((whizzy . "section -advi \"advi -geometry 1270x1024 -fullwidth -html Start-Document\" -dvicopy dvicopy" ))
+ '((whizzy . "section -advi \"advi -geometry 1270x1024 -fullwidth \" -dvicopy dvicopy" ))
 )
