97066eb5f4bfbd2bc92069a4a8f708f053a05e45
Matthias Puech <puech@cs.unibo.it>
2012-02-13 17:10:46 +0100

    Gasp: better example

--- experiments/fstorder/gasp.ml
+++ experiments/fstorder/gasp.ml
@@ -1,3 +1,43 @@
+(* First-order STLC *)
+(* let stlc = Slicer.init *)
+(* <:sign< *)
+(*   tp : type. *)
+(*   base : tp. *)
+(*   arr : tp -> tp -> tp. *)
+
+(*   tm : type. *)
+(*   lam : tm -> tm. *)
+(*   #app : tm -> tm -> tm. *)
+(*   s : tm -> tm. *)
+(*   z : tm. *)
+
+(*   env : type. *)
+(*   nil : env. *)
+(*   cons : tp -> env -> env. *)
+
+(*   is : env -> tm -> tp -> type. *)
+
+(*   is_weak : {E:env} {M:tm} {A:tp} {B:tp} *)
+(*     is E M B -> is (cons A E) (s M) B. *)
+(*   is_var : {E:env} {M:tm} {A:tp} *)
+(*     is (cons A E) z A. *)
+(*   is_app : {E:env} {M:tm} {N:tm} {A:tp} {B:tp} *)
+(*     is E M (arr A B) -> is E N A -> is E (app M N) B. *)
+(*   is_lam : {E:env} {M:tm} {A:tp} {B:tp} *)
+(*     is (cons B E) M A -> is E (lam M) A. *)
+(* >> *)
+
+(* let repo = Slicer.commit stlc *)
+(*   <:obj< *)
+(*     lam (lam (app (s z) z)) *)
+(*   >> *)
+(* ;; *)
+
+(* Format.printf "%a " Repo.Printer.t repo *)
+(* ;; *)
+
+
+(* HOAS STLC *)
 let stlc = Slicer.init
 <:sign<
   tp : type.
@@ -5,32 +45,34 @@ let stlc = Slicer.init
   arr : tp -> tp -> tp.
 
   tm : type.
-  lam : tm -> tm.
-  #app : tm -> tm -> tm.
-  s : tm -> tm.
-  z : tm.
+  lam : (tm -> tm) -> tm.
+  app : tm -> tm -> tm.
 
-  env : type.
-  nil : env.
-  cons : tp -> env -> env.
+  is : tm -> tp -> type.
 
-  is : env -> tm -> tp -> type.
-
-  is_weak : {E:env} {M:tm} {A:tp} {B:tp}
-    is E M B -> is (cons A E) (s M) B.
-  is_var : {E:env} {M:tm} {A:tp}
-    is (cons A E) z A.
-  is_app : {E:env} {M:tm} {N:tm} {A:tp} {B:tp}
-    is E M (arr A B) -> is E N A -> is E (app M N) B.
-  is_lam : {E:env} {M:tm} {A:tp} {B:tp}
-    is (cons B E) M A -> is E (lam M) A.
+  is_app : {M:tm} {N:tm} {A:tp} {B:tp}
+    is M (arr A B) -> is N A -> is (app M N) B.
+  is_lam : {M:tm -> tm} {A:tp} {B:tp}
+    ({x : tm} is x B -> is (M x) A) -> is (lam ([a] M a)) A.
 >>
-
-let repo = Slicer.commit stlc
-  <:obj<
-    lam (lam (app (s z) z))
-  >>
 ;;
 
-Format.printf "%a " Repo.Printer.t repo
+let m =
+<:obj<
+  lam ([x] lam ([y] app (app x y) y))
+>>
 ;;
+
+let m' = match LF.Strat.term stlc.Repo.sign [] m with
+  | LF.Strat.Obj m -> m
+  | _ -> assert false
+;;
+
+Format.printf "LF: %a@." LF.Printer.obj m'
+;;
+
+let repo = Slicer.commit stlc m
+;;
+
+(* Format.printf "%a " Repo.Printer.t repo *)
+(* ;; *)
