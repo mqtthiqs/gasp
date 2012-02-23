9f71463ae3567fe9978705fa49aceb3ee3d9ea3c
Matthias Puech <puech@cs.unibo.it>
2012-02-23 17:33:38 +0100

    Test: some rearrangement

--- experiments/fstorder/test2.ml
+++ experiments/fstorder/test2.ml
@@ -1,11 +1,10 @@
 #use "load.ml"
 
-let catch2 f x y =
-  try f x y
-  with Kernel.Conv.Not_conv (repo, m1, m2) as e ->
-    Format.printf "Not convertible: @[%a@] <> @[%a@] in @[%a@]@."
-      LF.Printer.obj m1 LF.Printer.obj m2 Repo.Printer.t repo;
-    raise e
+#trace Kernel.Check.obj
+#trace Kernel.Conv.obj
+#trace LF.Subst.obj
+#trace LF.Subst.fam
+#trace LF.Lift.fam
 
 (* HOAS STLC *)
 let repo = Slicer.init
@@ -13,13 +12,10 @@ let repo = Slicer.init
   tp : type.
   #base : tp.
   arr : tp -> tp -> tp.
-
   tm : type.
   lam : (tm -> tm) -> tm.
   app : tm -> tm -> tm.
-
   is : tm -> tp -> type.
-
   is_app : {M:tm} {N:tm} {A:tp} {B:tp}
     is M (arr A B) -> is N A -> is (app M N) B.
   is_lam : {M:tm -> tm} {A:tp} {B:tp}
@@ -28,42 +24,39 @@ let repo = Slicer.init
 >>
 ;;
 
-(* a term *)
+let test_commit repo m =
+  let repo = Slicer.commit repo m in
+  let m = LF.Strat.obj repo.Repo.sign [] m in
+  let n = LF.Strat.obj repo.Repo.sign [] (Slicer.checkout repo) in
+  Kernel.Conv.obj repo (m, n)
+;;
 
-let repo = Slicer.commit repo
+(* a term *)
+test_commit repo
 <<
   lam [x] lam [y] app (app x y) y
 >>
 ;;
 
-let m = Slicer.checkout repo
-;;
-
-(* a derivation *)
-
-let repo = Slicer.commit repo
+(* some derivations *)
+test_commit repo
 <<
   is_lam ([x] x) base base ([x] [H] H)
 >>
 ;;
 
-#trace Kernel.Check.obj
-#trace Kernel.Conv.obj
-#trace LF.Subst.obj
-#trace LF.Subst.fam
-
-let repo = Slicer.commit repo
+test_commit repo
 <<
   is_lam ([x] app x x) base base
   [x] [H] sorry (app x x) base
 >>
 ;;
 
-let repo = Slicer.commit repo
+test_commit repo
 <<
   is_lam ([x] lam [y] y) (arr base base) base
   [x] [H1] is_lam ([y] x) base base ([y] [H2] H2)
 >>
 ;;
 
-33
+42
