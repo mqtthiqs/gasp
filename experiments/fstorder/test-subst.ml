95f94c494f76f33bfc3a8bb3e4168ce52eaa8158
Matthias Puech <puech@cs.unibo.it>
2012-02-23 14:28:08 +0100

    Tests: better tests for substitutions

--- experiments/fstorder/test-subst.ml
+++ experiments/fstorder/test-subst.ml
@@ -2,6 +2,10 @@
 
 #trace Kernel.Check.obj
 #trace LF.Subst.fam
+#trace LF.Subst.obj
+#trace LF.Subst.kind
+#trace LF.Subst.spine
+#trace LF.Lift.obj
 
 let repo = Slicer.init
 <:sign<
@@ -14,28 +18,7 @@ let repo = Slicer.init
 
   p : {x : A} {y : A} P (f x y x).
   Q : P (f a b b) -> type.
-  (* q : Q (p b b). *)
 >>
-;;
-
-let m = LF.Subst.obj 0
-  (LF.Strat.obj repo.Repo.sign []
-   << [x] [y] f x y >>)
-  (match LF.Strat.obj repo.Repo.sign []
-   << [f] f a b >>
-   with
-     | LF.OLam (_, m) -> m
-     | _ -> assert false)
-;;
-
-let m = LF.Subst.obj 0
-  (LF.Strat.obj repo.Repo.sign []
-   << a >>)
-  (match LF.Strat.obj repo.Repo.sign []
-   << [x] [y] f x y >>
-   with
-     | LF.OLam (_, m) -> m
-     | _ -> assert false)
 ;;
 
 let repo = Slicer.commit repo
@@ -44,4 +27,50 @@ let repo = Slicer.commit repo
 >>
 ;;
 
-33
+(* ([x] n) [p <- m] *)
+let test_subst m n p =
+  let p = LF.Strat.obj repo.Repo.sign [] p in
+  let m = LF.Strat.obj repo.Repo.sign [] m in
+  let n = match LF.Strat.obj repo.Repo.sign [] n with
+    | LF.OLam (_, n) -> n
+    | _ -> assert false in
+  let q = LF.Subst.obj 0 m n in
+  Kernel.Conv.obj repo (p, q)
+;;
+
+test_subst
+<< a >>
+<< [X] [y] f X y >>
+<< [y] f a y >>
+;;
+
+test_subst
+<< [x] [y] x >>
+<< [X] X a b >>
+<< a >>
+;;
+
+test_subst
+<< [x] [y] f x y >>
+<< [X] X a b >>
+<< f a b >>
+;;
+
+test_subst
+<< [x] x >>
+<< [X] [y] X y>>
+<< [x] x >>
+;;
+
+test_subst
+<< [x] [y] x >>
+<< [X] [z] X z a >>
+<< [z] z >>
+;;
+
+test_subst
+<< [a] ?X[a] >>
+<< [X] [x] X x >>
+<< [x] ?X[x] >>
+;;
+
