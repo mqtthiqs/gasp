7e13706c7658d344d32e4ffcd02f928477dd042e
Matthias Puech <puech@cs.unibo.it>
2012-02-29 17:19:26 +0100

    Load: remove inexistent printer

--- experiments/fstorder/load.ml
+++ experiments/fstorder/load.ml
@@ -9,7 +9,6 @@
 #install_printer SLF.Printer.obj
 #install_printer SLF.Printer.fam
 #install_printer SLF.Printer.kind
-#install_printer SLF.Printer.entity
 #install_printer SLF.Printer.sign
 #install_printer SLF.Printer.env
 #install_printer SLF.Printer.context
