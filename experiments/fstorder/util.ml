837dbdf79ee4cfdf1508271d8f1cc81bf28a33d5
Matthias Puech <puech@cs.unibo.it>
2012-03-08 17:07:16 +0100

    Util: more functions on lists and options

--- experiments/fstorder/util.ml
+++ experiments/fstorder/util.ml
@@ -25,6 +25,21 @@ module List = struct
     | [] -> []
     | x :: xs -> f i x :: map_i (succ i) f xs
 
+  let transpose l =
+    let rec skip i l = if i=0 then l else None :: skip (pred i) l in
+    let rec inv i j = function
+      | [] -> []
+      | x :: xs -> skip (x-j) (Some i :: inv (succ i) (x+1) xs)
+    in inv 0 0 l
+
+  let drop xs is =
+    let rec aux k = function
+      | [], [] -> []
+      | x :: xs, i :: is when i=k -> x :: aux (succ k) (xs, is)
+      | x :: xs, is -> aux (succ k) (xs, is)
+      | _ -> failwith "drop"
+    in aux 0 (xs, is)
+
 end
 
 let (@@) a b = a b
@@ -47,4 +62,7 @@ module Option = struct
   let map f = function
     | None -> None
     | Some x -> Some (f x)
+  let default a = function
+    | Some y -> y
+    | _ -> a
 end
