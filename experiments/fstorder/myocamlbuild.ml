e69d8cdc1c8f27a27162aac57b2f0fa786272052
Matthias Puech <puech@cs.unibo.it>
2012-03-13 14:39:23 +0100

    Build system: launch tests with ocamlbuild, not make
    We generate .log files

--- experiments/fstorder/myocamlbuild.ml
+++ experiments/fstorder/myocamlbuild.ml
@@ -7,5 +7,9 @@ dispatch begin function
     pflag_and_dep ["ocaml"; "ocamldep"] "pa" (fun pa -> S[A"-ppopt"; P pa]);
     pflag_and_dep ["ocaml"; "compile"] "pa" (fun pa -> S[A"-ppopt"; P pa]);
     pdep [] "dep" (fun d -> [d]);
+    rule ".log" ~prod: "%.log" ~dep: "%.byte" (fun env _ ->
+      let byte = env "%.byte" in let log = env "%.log" in
+      Cmd(Sh ("./"^byte^" > "^log))
+    )
   | _ -> ()
 end
