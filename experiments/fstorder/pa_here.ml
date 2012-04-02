b1ce600cdd3a2f1186f7209e713856cf43fe926e
Matthias Puech <puech@cs.unibo.it>
2012-04-02 16:32:06 +0200

    Pa_here: new Caml syntax extension module to fetch current location
    use it in all test files

---
+++ experiments/fstorder/pa_here.ml
@@ -0,0 +1,27 @@
+open Camlp4.PreCast
+
+EXTEND Gram Syntax.expr:
+  [ [ "HERE" ->
+    <:expr< Camlp4.PreCast.($Ast.Meta.MetaLoc.meta_loc_expr _loc _loc$) >>
+  ] ];
+END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
