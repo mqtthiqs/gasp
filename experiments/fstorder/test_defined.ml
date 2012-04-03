9cfdfeec21dbbba8036c93da74f72bf4f9f4a2b4
Matthias Puech <puech@cs.unibo.it>
2012-04-03 20:28:00 +0200

    Test_defined: adapt to new interface with Kernel.eval.
    * we suppress reduce (which was doing much too much work)
    * every match becomes a recursive function with a call to Kernel.eval in the default case
    * also we add mult and emult, and some more tests

--- experiments/fstorder/test_defined.ml
+++ experiments/fstorder/test_defined.ml
@@ -1,12 +1,6 @@
 #use "load.ml"
 ;;
 
-let reduce repo env m =
-  Format.printf "**** reduce: %a@." SLF.Printer.term m;
-  let env = SLF.Strat.env repo.Struct.Repo.sign env in
-  let repo = Slicer.commit repo env m in
-  Slicer.checkout repo
-
 let repo = Slicer.init <:sign<
   nat : type.
   #o : nat.
@@ -14,20 +8,34 @@ let repo = Slicer.init <:sign<
 
   two : nat = s (s o).
 
-  plus : {m:nat} {n:nat} nat = $ fun m n -> match m with
+  plus : nat -> nat -> nat = $ fun m n ->
+    let rec f = function
     | << o >> -> n
-    | << s $m$ >> -> << s (plus $m$ $n$) >>
-    | << $m$ >> -> << plus $reduce repo <:env< >> m$ $n$ >>
+    | << s $m$ >> -> << s (plus $m$ $n$) >> (* or << s (plus $m$ $n$) >> *)
+    | default -> f (Kernel.eval repo env default)
+    in f m
+  $.
+
+  mult: nat -> nat -> nat = $ fun m n ->
+    let rec f = function
+      | << o >> -> << o >>
+      | << s $m$ >> -> << plus $n$ (mult $m$ $n$) >>
+      | default -> f (Kernel.eval repo env default)
+    in f m
   $.
 
   exp : type.
   enat : nat -> exp.
   eplus : exp -> exp -> exp.
+  emult : exp -> exp -> exp.
 
-  eval : {e : exp} nat = $ fun e -> match e with
-    | << enat $n$ >> -> n
-    | << eplus $e1$ $e2$ >> -> << plus (eval $e1$) (eval $e2$) >>
-    | << $e$ >> -> reduce repo <:env< >> << eval $e$ >>
+  eval : {e : exp} nat = $ fun e ->
+    let rec f = function
+      | << enat $n$ >> -> n
+      | << eplus $e1$ $e2$ >> -> << plus (eval $e1$) (eval $e2$) >>
+      | << emult $e1$ $e2$ >> -> << mult (eval $e1$) (eval $e2$) >>
+      | default -> f (Kernel.eval repo env default)
+    in f e
   $.
 >>
 ;;
@@ -39,12 +47,16 @@ Tests.conv repo <<
 >> << nat >>
 ;;
 
-
 Tests.conv repo <<
   plus o two
 >> <<
   s (s o)
 >> << nat >>
+;;
+
+Tests.commit repo <<
+  plus two o
+>>
 ;;
 
 Tests.conv repo <<
@@ -63,6 +75,35 @@ Tests.conv repo <<
 
 Tests.commit repo <<
   plus two two
+>>
+;;
+
+Tests.commit_eq repo <<
+  mult two two
+>> <<
+  plus two two
+>>
+;;
+
+Tests.commit_eq repo <<
+  plus (plus two two) (plus o (s o))
+>> <<
+  s (s (s (s (s o))))
+>>
+;;
+
+Tests.commit_eq repo <<
+  plus (s o) (mult two (s two))
+>> <<
+  s (s (s (s (s (s (s o))))))
+>>
+;;
+
+Tests.commit_eq repo <<
+  eval (eplus (enat (s o)) (emult (enat two) (enat (s two)))
+)
+>> <<
+  plus (s o) (mult two (s two))
 >>
 ;;
 
