8259cf79ece4c7bd2978ce211701cd0917937cdc
Matthias Puech <puech@cs.unibo.it>
2012-04-17 16:59:31 +0200

    Test_recmatch: test of Pa_recmatch

---
+++ experiments/fstorder/test_recmatch.ml
@@ -0,0 +1,20 @@
+let eval x y = Some 42
+
+(* usual match *)
+let _ =
+  match None with
+      Some x -> Some x
+    | None -> None
+
+(* recursive match *)
+let _ =
+  match* None in true with
+    | Some x -> x
+
+(* usual let *)
+let _ =
+  let (x,y) = (1,2) in x
+
+(* recursive let *)
+let _ =
+  let* Some a in true = None in a
