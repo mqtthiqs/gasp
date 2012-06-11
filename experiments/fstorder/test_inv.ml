c880ff378be33b57014570b1effb247f697854aa
Matthias Puech <puech@cs.unibo.it>
2012-06-11 19:42:02 +0200

    Test_inv: add test failing because of missed f^0

--- experiments/fstorder/test_inv.ml
+++ experiments/fstorder/test_inv.ml
@@ -13,6 +13,15 @@ let repo = Version.init
   c : C.
   P : A -> type.
   Q : {x : A} P x -> type.
+
+  T : type.
+  lam : (T -> T) -> T.
+  id : T -> T = $ fun x -> return x $.
+  f : T -> T = $ fun t -> match* t with
+    | << lam $t$ >> ->
+        match* << lam [x] f ($t$ (f^0 x x)) >> with
+          | t -> return t
+  $.
 >>
 ;;
 
@@ -41,6 +50,27 @@ Tests.inv repo 1 <<
   {x : A} {p : P x} Q x p
 >> <<
   {x : A} {p : P x} Q x p -> P x
+>>
+;;
+
+(* This works without problems *)
+Tests.commit_eq repo <<
+  f (lam [x] x)
+>> <<
+  lam [x] x
+>>
+;;
+
+(* But if we insert an id on the way, it hides the reduction of f (f^0 x x) -> x: *)
+(* Steps:
+   - f (lam [x] id x)
+   - lam [x] f (id (f^0 x x))
+   - lam [x] f x
+ *)
+Tests.commit_eq repo <<
+  f (lam [x] id x)
+>> <<
+  lam [x] x
 >>
 ;;
 
