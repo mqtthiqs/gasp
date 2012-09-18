4f2c89a63f9b29c66578b2bafbded0450279794f
Matthias Puech <puech@cs.unibo.it>
2012-09-18 20:04:13 +0200

    add .dir_locals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((eval . (setq default-directory (locate-dominating-file
+                                          buffer-file-name
+                                          ".dir-locals.el")
+                       tags-file-name (concat default-directory
+                                          "TAGS")
+)))))
